e61b90660458da181adaf448ed9b6631ab7ebbca
Denis Kozlov <yadenisko1989@gmail.com>
2025-04-15 20:44:39 +0300

    initial structure

---
+++ exchange/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404, redirect
+
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+# Регистрация пользователя
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('item_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
