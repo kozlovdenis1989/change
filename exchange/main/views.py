92d0ec6f216c706e3e848bd99596cfc9dd36bc7d
Denis Kozlov <yadenisko1989@gmail.com>
2025-04-16 17:51:18 +0300

    change /home, add paginator, add managments

--- exchange/main/views.py
+++ exchange/main/views.py
@@ -1,7 +1,9 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
-
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+
+from .models import Item, ExchangeProposal
 
 # Регистрация пользователя
 def register(request):
@@ -15,7 +17,11 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 
-# Главная, вещи
+# Главная
 def home(request):
-    content = None
-    return render(request, 'index.html', content)
+    items_list = Item.objects.all().order_by('-created_at')
+    paginator = Paginator(items_list, 6)  
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'index.html', {'page_obj': page_obj})
